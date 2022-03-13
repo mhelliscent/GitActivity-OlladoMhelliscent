49101cf195b6244b3c32f9b098b7116f4f91f3ab
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-03-14 03:04:40 +0800

    My Educational Background added

--- git-python-ollado.py
+++ git-python-ollado.py
@@ -8,3 +8,7 @@ Created on Mon Mar 14 02:52:38 2022
 print("Mhelliscent M. Ollado")
 print("Balogo, Boac, Marinduque")
 print("May 20, 2002")
+print("College of Engineering, Information and Industrial Technology")
+print("Bachelor of Science in Information Technology")
+print("Second year college")
+print("Section D")
