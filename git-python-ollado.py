166565b1da4f6a3de9b24ca5042ef436f44e924c
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-03-14 02:59:20 +0800

    My Personal Info added

---
+++ git-python-ollado.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 02:52:38 2022
+
+@author: windows 10
+"""
+
+print("Mhelliscent M. Ollado")
+print("Balogo, Boac, Marinduque")
+print("May 20, 2002")
